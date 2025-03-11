ef47789f27ec43801c126f196e42c1da4acc9dcd
Enapoles02 <enrique.napoles@dbschenker.com>
2025-03-11 11:43:14 -0600

    Update enrique.py

--- enrique.py
+++ enrique.py
@@ -305,8 +305,8 @@ def show_main_app():
             feelings = {"😃": "Feliz", "😐": "Normal", "😔": "Triste", "😡": "Molesto", "😴": "Cansado", "🤒": "Enfermo"}
             selected_feeling = st.radio("Selecciona tu estado de ánimo:", list(feelings.keys()))
             health_problem = st.radio("❓ ¿Te has sentido con problemas de salud esta semana?", ["Sí", "No"])
-            st.write("Nivel de energía (elige entre 10, 20, 30, 40 o 50):")
-            energy_options = [10, 20, 30, 40, 50]
+            st.write("Nivel de energía")
+            energy_options = [20, 40, 60, 80, 100]
             energy_level = st.radio("Nivel de energía:", options=energy_options, horizontal=True)
             battery_html = f"""
             <div style="display: inline-block; border: 2px solid #000; width: 40px; height: 100px; position: relative;">
