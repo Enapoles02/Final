85d8def420fafa20249e87c13a1bb03eb58d382a
Enapoles02 <enrique.napoles@dbschenker.com>
2025-05-21 11:49:35 -0600

    Update enrique.py

--- enrique.py
+++ enrique.py
@@ -63,7 +63,7 @@ valid_users = {
     # Perfil KPI (este usuario mostrará el dashboard de KPIs)
     "KPI": "KPI Reporte"
     # OTC TL
-    "MHORTA": "Monica Horta",
+    "MOHORTA": "Monica Horta",
     "JAVILA": "Julio Avila",
     "OTCLEAD": "OTC LEAD",
     "EAVILA": "Edson Avila",
